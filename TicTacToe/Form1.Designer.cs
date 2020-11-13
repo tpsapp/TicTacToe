a6224e812859ad0cdb8dddd6531b90313347f1c0
Thomas Sapp <tpsapp@hotmail.com>
2020-11-13 07:05:08 -0700

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace TicTacToe
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmsMain
+            // 
+            this.cmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNewGame,
+            this.toolStripSeparator1,
+            this.tsmiExit});
+            this.cmsMain.Name = "contextMenuStrip1";
+            this.cmsMain.Size = new System.Drawing.Size(133, 54);
+            // 
+            // tsmiNewGame
+            // 
+            this.tsmiNewGame.Name = "tsmiNewGame";
+            this.tsmiNewGame.Size = new System.Drawing.Size(132, 22);
+            this.tsmiNewGame.Text = "New &Game";
+            this.tsmiNewGame.Click += new System.EventHandler(this.tsmiNewGame_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(129, 6);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(132, 22);
+            this.tsmiExit.Text = "E&xit";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 85);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 85);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(190, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 85);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 85);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.Enabled = false;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(94, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 85);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.Enabled = false;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(190, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 85);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.Enabled = false;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(0, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 85);
+            this.label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.Enabled = false;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(94, 174);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 85);
+            this.label8.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.Enabled = false;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(190, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 85);
+            this.label9.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(282, 260);
+            this.ContextMenuStrip = this.cmsMain;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.cmsMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip cmsMain;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNewGame;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class frmMain : Form
+    {
+        int[] iBoard = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int iTurn = 1; // X = 1, O = 2
+        bool bFinished = false;
+
+        private void initBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                iBoard[i] = 0;
+            }
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+        }
+
+        private void changePlayer()
+        {
+            switch (iTurn)
+            {
+                case 1:
+                    iTurn = 2;
+                    break;
+                case 2:
+                    iTurn = 1;
+                    break;
+            }
+        }
+
+        private string getPlayer()
+        {
+            switch (iTurn)
+            {
+                case 1:
+                    return "X";
+                case 2:
+                    return "O";
+            }
+            return "!";
+        }
+
+        private void placePiece(int x, int y)
+        {
+            if (x < 93 && y < 86)
+            {
+                if (iBoard[0] == 0)
+                {
+                    iBoard[0] = iTurn;
+                    label1.Text = getPlayer();
+                }
+            }
+            else if (x > 93 && x < 189 && y < 86)
+            {
+                if (iBoard[1] == 0)
+                {
+                    iBoard[1] = iTurn;
+                    label2.Text = getPlayer();
+                }
+            }
+            else if (x > 189 && y < 86)
+            {
+                if (iBoard[2] == 0)
+                {
+                    iBoard[2] = iTurn;
+                    label3.Text = getPlayer();
+                }
+            }
+            else if (x < 93 && y > 86 && y < 173)
+            {
+                if (iBoard[3] == 0)
+                {
+                    iBoard[3] = iTurn;
+                    label4.Text = getPlayer();
+                }
+            }
+            else if (x > 93 && x < 189 && y > 86 && y < 173)
+            {
+                if (iBoard[4] == 0)
+                {
+                    iBoard[4] = iTurn;
+                    label5.Text = getPlayer();
+                }
+            }
+            else if (x > 189 && y > 86 && y < 173)
+            {
+                if (iBoard[5] == 0)
+                {
+                    iBoard[5] = iTurn;
+                    label6.Text = getPlayer();
+                }
+            }
+            else if (x < 93 && y > 173)
+            {
+                if (iBoard[6] == 0)
+                {
+                    iBoard[6] = iTurn;
+                    label7.Text = getPlayer();
+                }
+            }
+            else if (x > 93 && x < 189 && y > 173)
+            {
+                if (iBoard[7] == 0)
+                {
+                    iBoard[7] = iTurn;
+                    label8.Text = getPlayer();
+                }
+            }
+            else if (x > 189 && y > 173)
+            {
+                if (iBoard[8] == 0)
+                {
+                    iBoard[8] = iTurn;
+                    label9.Text = getPlayer();
+                }
+            }
+        }
+
+        private void checkVictory()
+        {
+            if (iBoard[0] != 0)
+            {
+                if (iBoard[0] == iBoard[1] && iBoard[1] == iBoard[2])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+                if (iBoard[0] == iBoard[3] && iBoard[3] == iBoard[6])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+            }
+            if (iBoard[4] != 0)
+            {
+                if (iBoard[3] == iBoard[4] && iBoard[4] == iBoard[5])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+                if (iBoard[1] == iBoard[4] && iBoard[4] == iBoard[7])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+                if (iBoard[0] == iBoard[4] && iBoard[4] == iBoard[8])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+                if (iBoard[2] == iBoard[4] && iBoard[4] == iBoard[6])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+            }
+            if (iBoard[8] != 0)
+            {
+                if (iBoard[6] == iBoard[7] && iBoard[7] == iBoard[8])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+                if (iBoard[2] == iBoard[5] && iBoard[5] == iBoard[8])
+                {
+                    bFinished = true;
+                    MessageBox.Show(getPlayer() + " has won the game!", "Victory!");
+                    iTurn = 1;
+                    initBoard();
+                    return;
+                }
+            }
+            int iDraw = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (iBoard[i] != 0)
+                {
+                    iDraw++;
+                }
+            }
+            if (iDraw == 9)
+            {
+                bFinished = true;
+                MessageBox.Show("The game is a draw!", "Draw!");
+                iTurn = 1;
+                initBoard();
+                return;
+            }
+            changePlayer();
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                placePiece(e.X, e.Y);
+                checkVictory();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiNewGame_Click(object sender, EventArgs e)
+        {
+            iTurn = 1;
+            initBoard();
+        }
+    }
+}
